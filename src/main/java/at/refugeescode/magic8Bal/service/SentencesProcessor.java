8323f98433963bca1043e78c0b5b7be693b0914b
mohammmod <mohammad.sawas2@outlook.com>
2018-04-17 00:44:56 +0200

    clean code

--- src/main/java/at/refugeescode/magic8Bal/service/SentencesProcessor.java
+++ src/main/java/at/refugeescode/magic8Bal/service/SentencesProcessor.java
@@ -9,7 +9,12 @@ import java.util.Random;
 @Service
 public class SentencesProcessor {
     public String giveOneSentence() {
-        List<String> sentences = new ArrayList<>(8);
+        List<String> sentencesInitilaized = new ArrayList<>(8);
+        List<String> sentences = addSentences(sentencesInitilaized);
+        return getRandomSentence(sentences);
+    }
+
+    private List<String> addSentences(List<String> sentences) {
         sentences.add("do you like donut?");
         sentences.add("what is your favorite cooler");
         sentences.add("do you like the weather");
@@ -18,8 +23,7 @@ public class SentencesProcessor {
         sentences.add("where are you going to spend your vacation");
         sentences.add("if you have chance to work at microsoft here would you like ");
         sentences.add("are you tired ");
-
-        return getRandomSentence(sentences);
+        return sentences;
     }
 
     private String getRandomSentence(List<String> sentences) {
